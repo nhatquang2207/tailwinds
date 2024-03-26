0b25f635cb69c03261b82ae72e371c96dd95be16
nhatquang2207 <nhatquangkute12@gmail.com>
2024-03-26 14:03:31 +0700

    design finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import Design1 from './component/design1';
-
+import Design1 from "./component/design1";
 
 function App() {
-
   return (
-    <>  
-      <div className='bg-blue-300 h-screen flex justify-center items-center  '>
-        <Design1/>      </div>
+    <>
+      <div className="bg-blue-300 h-screen flex justify-center items-center  ">
+        <Design1 />{" "}
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/component/design1.tsx
+++ src/component/design1.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 function Design1() {
   return (
