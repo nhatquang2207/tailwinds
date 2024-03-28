87b1ae37ffd52d6aec9f08e6674437dbb02c1e3b
nhatquang2207 <nhatquangkute12@gmail.com>
2024-03-28 14:02:17 +0700

    design3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Design2 from "./component/desing2";
+// import Design2 from "./component/design2/desing2";
+import Design3 from "./component/design3/design3";
 
 function App() {
   return (
     <>
-      <div className="bg-blue-300 h-screen flex justify-center items-center  ">
+      <div className="bg-blue-300  flex  justify-center items-center  ">
         {/* <Design1 />{" "} */}
-        <Design2/>
+        {/* <Design2/> */}
+        <Design3/>  
 
       </div>
     </>
--- src/component/design3/design3.tsx
+++ src/component/design3/design3.tsx
@@ -1,10 +1,80 @@
-
-function design3() {
+import React from 'react'
+const tag = ['Profile',"New York","Relax","Preson","Fashion"]
+function Design3() {
   return (
-    <div>
-      
-    </div>
+    <div className='w-[60rem] p-6 rounded-lg mt-16 mb-16 bg-white  shadow-2xl'>
+
+     <p className=' font-bold text-3xl  '>Popular collection </p>
+
+       <div className="mt-5">
+            {tag.map(item=>(<button className='mr-4 bg-gray-600 p-2 rounded-lg hover:bg-gray-100'> {item} </button>))}
+
+       </div>
+       <div className='grid grid-cols-3'>
+         <div className='mt-3 grid grid-cols-1  w-[18rem]' >
+        <div className='bg-blue-200 '>
+          <img  className='p-3 w-full h-[12rem]  rounded-3xl' src='https://placewaifu.com/image/100' alt=''/>
+           <div className='grid grid-cols-3 gap-2 p-2'>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/410' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/200' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/300' alt=''/>
+           </div>
+          <div className='flex mr-2 ml-2'> 
+           <p className='grow'>People  </p>
+           <p className=''>44 img</p>
+           </div>
+        </div>
+       </div> 
+       <div className='mt-3 grid grid-cols-1  w-[18rem]' >
+        <div className='bg-blue-200 '>
+          <img  className='p-3 w-full h-[12rem]  rounded-3xl' src='https://placewaifu.com/image/100' alt=''/>
+           <div className='grid grid-cols-3 gap-2 p-2'>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/420' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/200' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/300' alt=''/>
+           </div>
+          <div className='flex mr-2 ml-2'> 
+           <p className='grow'>People  </p>
+           <p className=''>44 img</p>
+           </div>
+        </div>
+       
+       </div>
+       <div className='mt-3 grid grid-cols-1  w-[18rem]' >
+        <div className='bg-blue-200 '>
+          <img  className='p-3 w-full h-[12rem]  rounded-3xl' src='https://placewaifu.com/image/100' alt=''/>
+           <div className='grid grid-cols-3 gap-2 p-2'>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/420' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/200' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/300' alt=''/>
+           </div>
+          <div className='flex mr-2 ml-2'> 
+           <p className='grow'>People  </p>
+           <p className=''>44 img</p>
+           </div>
+        </div>
+       
+       </div>
+       <div className='mt-3 grid grid-cols-1  w-[18rem]' >
+        <div className='bg-blue-200 '>
+          <img  className='p-3 w-full h-[12rem]  rounded-3xl' src='https://placewaifu.com/image/100' alt=''/>
+           <div className='grid grid-cols-3 gap-2 p-2'>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/420' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/200' alt=''/>
+               <img className=' rounded-lg' src='https://placewaifu.com/image/300' alt=''/>
+           </div>
+          <div className='flex mr-2 ml-2'> 
+           <p className='grow'>People  </p>
+           <p className=''>44 img</p>
+           </div>
+        </div>
+       
+       </div> 
+       
+       </div>
+       
+      </div>
   )
 }
 
-export default design3
+export default Design3;
