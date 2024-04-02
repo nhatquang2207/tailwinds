b6b9661ea44a5472394a71fd0f6aa674bf765d10
nhatquang2207 <nhatquangkute12@gmail.com>
2024-04-02 16:32:07 +0700

    design6

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 //  import Design2 from "./component/design2/desing2";
 // import Design3 from "./component/design3/design3";
 import React from "react";
-import Design5 from "./component/design5/design5";
+import Design6 from "./component/design6/design6";
 
 function App() {
   return (
-
     <div className=" flex justify-center items-center  ">
       {/* <Design1 />{" "} */}
       {/* <Design2/> */}
       {/* <Design3 /> */}
-      <Design5 />
-
-
+      {/* <Design5 /> */}
+      <Design6 />
     </div>
-
   );
 }
 
